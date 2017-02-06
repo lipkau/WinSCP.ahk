1d9434051afc42e8533bad0383e0dbc27989b75d
Oliver Lipkau <oliver@lipkau.net>
2017-02-06 12:00:52 +0100

    added pause to see feedback

--- WinSCP_regDLL.cmd
+++ WinSCP_regDLL.cmd
@@ -2,3 +2,4 @@ cd "%~dp0"
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe WinSCPnet.dll /codebase WinSCPnet.dll
 %WINDIR%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe WinSCPnet.dll /codebase WinSCPnet.dll
+pause
