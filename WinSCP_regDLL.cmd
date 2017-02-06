3c9cbc2eae90baa96f35fbfeb762842e7727d6a9
Oliver Lipkau <oliver@lipkau.net>
2017-02-06 14:53:31 +0100

    fixed COM registration

--- WinSCP_regDLL.cmd
+++ WinSCP_regDLL.cmd
@@ -1,5 +1,11 @@
-cd "%~dp0"
+@echo off
+cls
+pushd "%~dp0bin\"
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe WinSCPnet.dll /codebase WinSCPnet.dll
-%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe WinSCPnet.dll /codebase WinSCPnet.dll
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe WinSCPnet.dll /codebase WinSCPnet.dll /tlb
+echo.
+echo.
+%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe WinSCPnet.dll /codebase WinSCPnet.dll /tlb
+echo.
+echo.
 pause
